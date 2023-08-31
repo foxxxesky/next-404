c5b0f16cdc6922d7947f858e94eed2a75d98cbf3
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-09-01 04:41:52 +0700

    Update SignUpForm.tsx

--- components/molecules/SignUpForm.tsx
+++ components/molecules/SignUpForm.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
+import axios from 'axios'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
 
+import { toast } from 'react-hot-toast'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
 
 const signUpSchema = z.object({
   username: z.string().min(3, { message: 'Username must be at least 3 characters.' }),
@@ -15,6 +19,9 @@ const signUpSchema = z.object({
 })
 
 export const SignUpForm = () => {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
   const form = useForm<z.infer<typeof signUpSchema>>({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
@@ -25,7 +32,21 @@ export const SignUpForm = () => {
   })
 
   const onSubmit = async (values: z.infer<typeof signUpSchema>) => {
-    console.log(values)
+    try {
+      setLoading(true)
+
+      const response = await axios.post('/api/auth/signup', values)
+
+      if (response.data.status === 400) throw new Error(response.data.error)
+
+      toast.success('Sign up success!')
+      router.push('/signin')
+
+    } catch (error: any) {
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
@@ -40,7 +61,12 @@ export const SignUpForm = () => {
                 <FormItem>
                   <FormLabel>Username</FormLabel>
                   <FormControl>
-                    <Input placeholder='Username' {...field} />
+                    <Input
+                      type='text'
+                      disabled={loading}
+                      placeholder='Username'
+                      {...field}
+                  />
                   </FormControl>
                   <FormMessage className='text-xs' />
                 </FormItem>
@@ -55,7 +81,12 @@ export const SignUpForm = () => {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input type='email' placeholder='@youremail' {...field} />
+                    <Input
+                      type='email'
+                      disabled={loading}
+                      placeholder='@youremail'
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage className='text-xs' />
                 </FormItem>
@@ -70,7 +101,12 @@ export const SignUpForm = () => {
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input type='password' placeholder='Username' {...field} />
+                    <Input
+                      type='password'
+                      disabled={loading}
+                      placeholder='password'
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage className='text-xs' />
                 </FormItem>
@@ -80,6 +116,7 @@ export const SignUpForm = () => {
           <div className="pt-6">
             <Button
               type='submit'
+              disabled={loading}
               className='w-full'
             >
               Sign Up

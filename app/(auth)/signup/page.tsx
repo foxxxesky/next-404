e69f4b01f3c6640070d3dc7a2e1f3afc3c4530a4
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-09-01 03:24:11 +0700

    update sign up page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -5,19 +5,38 @@ import Link from 'next/link'
 // import { axios } from 'axios'
 import { useRouter } from 'next/navigation'
 
-export default function SignInPage() {
-    const [ user, setUser ] = React.useState({
-        username: '',
-        email: '',
-        password: ''
-    })
+import { Button } from '@/components/ui/button'
+import { SignUpForm } from '@/components/molecules/SignUpForm'
 
-    const onSignUp = async () => {
-    }
+export default function SignUpPage() {
+  const [ user, setUser ] = React.useState({
+      username: '',
+      email: '',
+      password: ''
+  })
 
-    return (
-        <div>
-            <h1>Sign Up Page</h1>
-        </div>
-    )
+  const onSignUp = async () => {
+  }
+
+  return (
+    <div className="flex flex-col justify-center gap-8 md:w-1/3 md:px-10 px-4 py-12">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <h1 className='text-slate-800 font-bold text-2xl'>Sign Up</h1>
+        <p className='text-xs text-slate-400 font-medium'>Please create your account</p>
+      </div>
+
+      <SignUpForm />
+
+      <div className="flex justify-center items-center">
+        <p className='text-xs text-slate-400'>already have an account?</p>
+        <Button variant={'link'} className='p-0 ps-2 text-xs'>
+          <Link
+            href='/signin'
+          >
+            SignIn
+          </Link>
+        </Button>
+      </div>
+    </div>
+  )
 }

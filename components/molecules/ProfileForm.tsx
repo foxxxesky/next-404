e2faa19549bb266270449be2d228b6080994090a
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-09-04 15:03:36 +0700

    Update ProfileForm.tsx

--- components/molecules/ProfileForm.tsx
+++ components/molecules/ProfileForm.tsx
@@ -3,8 +3,6 @@
 import * as z from 'zod'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useRouter } from 'next/navigation'
-import { useSession } from 'next-auth/react'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import Link from 'next/link'
@@ -12,6 +10,7 @@ import { User } from '@prisma/client'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormLabel } from '@/components/ui/form'
+import { UserCircle2 } from 'lucide-react'
 
 interface ProfileFormProps {
   initialData: User
@@ -25,9 +24,7 @@ const updateProfileSchema = z.object({
 type updateProfileFormValues = z.infer<typeof updateProfileSchema>
 
 export const ProfileForm: React.FC<ProfileFormProps> = ({ initialData }) => {
-  const router = useRouter()
   const [loading, setLoading] = useState(false)
-  const { data: session, status } = useSession()
 
   const form = useForm<updateProfileFormValues>({
     resolver: zodResolver(updateProfileSchema),
@@ -40,61 +37,66 @@ export const ProfileForm: React.FC<ProfileFormProps> = ({ initialData }) => {
 
 
   return (
-    <div className='flex'>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(updateUserProfile)} className='space-y-4'>
-          <FormField
-            control={form.control}
-            name='username'
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='text'
-                      disabled={loading}
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )
-            }}
-          />
-          <FormField
-            control={form.control}
-            name='email'
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='text'
-                      disabled={loading}
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )
-            }}
-          />
+    <div className='md:flex gap-5 w-full justify-center'>
+      <div className='md:w-1/5 md:flex justify-center items-center'>
+        <UserCircle2 size={125} className='hidden md:flex text-slate-600' />
+      </div>
+      <div className='md:w-2/5'>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(updateUserProfile)} className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='username'
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='text'
+                        disabled={loading}
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )
+              }}
+            />
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='text'
+                        disabled={loading}
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )
+              }}
+            />
 
-          <div className='pt-4 flex gap-4'>
-            <Button
-              variant='secondary'
-            >
-              <Link href='/'>Back</Link>
-            </Button>
-            <Button
-              disabled={loading}
-              type='submit'
-            >
-              Update Profile
-            </Button>
-          </div>
-        </form>
-      </Form>
+            <div className='pt-4 flex gap-4'>
+              <Button
+                variant='secondary'
+              >
+                <Link href='/'>Back</Link>
+              </Button>
+              <Button
+                disabled={loading}
+                type='submit'
+              >
+                Update Profile
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   )
 }

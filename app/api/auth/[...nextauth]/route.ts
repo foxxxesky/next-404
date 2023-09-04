341c6a0c088f45985153f793b5f6c923e33168c7
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-09-04 15:02:33 +0700

    update next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,62 +1,6 @@
-import bcrypt from 'bcryptjs'
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import CredentialsProvider from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth'
+import { options } from './options'
 
-import prisma from '@/db'
-
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET!,
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-    }),
-    CredentialsProvider({
-      type: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', placeholder: 'Enter Email' },
-        password: { label: 'Password', placeholder: 'password' },
-      },
-
-      async authorize(credentials: any) {
-        const { email, password } = credentials as {
-          email: string
-          password: string
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email,
-          },
-        })
-
-        if (!user) {
-          return null
-        }
-
-        const passwordValid = await bcrypt.compare(password, user.password)
-
-        if (!passwordValid) {
-          return null
-        }
-
-        return user
-      }
-    })
-  ],
-  // jwt: {
-
-  // }
-  pages: {
-    signIn: '/signin'
-  }
-}
-
-const handler = NextAuth(authOptions)
+const handler = NextAuth(options)
 
 export { handler as GET, handler as POST }

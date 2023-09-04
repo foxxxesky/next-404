e40f86613a7cfb45175688f80a812ee4be2c6724
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-09-04 10:53:19 +0700

    update root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,17 +5,15 @@ const RootLayout = ({ children } : {
   children: React.ReactNode
 }) => {
   return (
-    <div className="h-full">
-      <main>
-        <Navbar />
-        <div className='hidden md:flex mt-16 w-120 flex-col fixed inset-y-0'>
-          <Sidebar />
-        </div>
-        <div className='md:pl-52 pt-16 h-full'>
-          { children }
-        </div>
-      </main>
-    </div>
+    <main className='h-full overflow-x-hidden'>
+      <Navbar />
+      <div className='hidden md:flex mt-16 w-120 flex-col fixed inset-y-0'>
+        <Sidebar />
+      </div>
+      <div className='md:pl-52 pt-16 h-full'>
+        { children }
+      </div>
+    </main>
   )
 }
 

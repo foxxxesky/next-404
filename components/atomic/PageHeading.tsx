12fa750634bc4e3f6cd9d227497ba1cd4a4d5c33
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-09-04 10:54:48 +0700

    create page heading components

---
+++ components/atomic/PageHeading.tsx
@@ -0,0 +1,22 @@
+import { Separator } from '@/components/ui/separator'
+
+interface PageHeadingProps {
+  title: string
+  desc?: string
+}
+
+const PageHeading: React.FC<PageHeadingProps> = ({
+  title,
+  desc
+ }) => {
+  return (
+    <div className="space-y-2">
+      <h1 className="font-bold text-2xl text-slate-800">{ title }</h1>
+      <p className="font-medium text-sm text-slate-400">{ desc }</p>
+      <Separator />
+    </div>
+
+  )
+}
+
+export default PageHeading

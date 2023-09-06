a5eae9d68425bb360e8a0e9d1be7c8555f08cfaa
foxxxesky <abdurrahman1467@gmail.com>
2023-09-06 13:22:06 +0700

    create admin  page

---
+++ app/(root)/(routes)/admin/page.tsx
@@ -0,0 +1,13 @@
+import PageHeading from '@/components/atomic/PageHeading'
+
+const AdminPage = () => {
+  return (
+    <div className="p-4">
+      <PageHeading
+        title='Admin'
+      />
+    </div>
+  )
+}
+
+export default AdminPage

7a44296182110f40602352ea4782d8bda5459bf2
foxxxesky <abdurrahman1467@gmail.com>
2023-09-06 15:05:31 +0700

    Update PageHeading.tsx

--- components/atomic/PageHeading.tsx
+++ components/atomic/PageHeading.tsx
@@ -11,7 +11,7 @@ const PageHeading: React.FC<PageHeadingProps> = ({
  }) => {
   return (
     <div className="space-y-2">
-      <h1 className="font-bold text-2xl text-slate-800">{ title }</h1>
+      <h1 className="font-bold text-3xl text-slate-800">{ title }</h1>
       <p className="font-medium text-sm text-slate-400">{ desc }</p>
       <Separator />
     </div>
